4450855d55a3cc8f20a848aec08439eb9d869524
Vincent De Comarmond <vincent.de.comarmond.sa@gmail.com>
2024-07-03 12:53:59 +0200

    The way I would have done things

--- bash/prime-factors/prime_factors.sh
+++ bash/prime-factors/prime_factors.sh
@@ -1,16 +1,2 @@
 #!/usr/bin/env bash
-main() {
-	local current=2 remaining="$1"
-	local -a factors
-
-	while ((remaining != 1)); do
-		if ((remaining % current == 0)); then
-			((remaining /= current))
-			factors+=("$current")
-		else
-			((current == 2)) && ((current = 3)) || ((current += 2))
-		fi
-	done
-	echo "${factors[@]}"
-}
-main "$1"
+factor "$1" | cut -d":" -f2- | cut -c2-
