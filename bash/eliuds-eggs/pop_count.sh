232f3726759a12531c2f0ae0139051307496710d
Vincent De Comarmond <vincent.de.comarmond.sa@gmail.com>
2024-07-10 20:38:06 +0200

    Solved many problems

--- bash/eliuds-eggs/pop_count.sh
+++ bash/eliuds-eggs/pop_count.sh
@@ -3,7 +3,7 @@ main() {
 	# Octal and binary are friends
 	local -a oct_2_bin=(0 1 1 2 1 2 2 3)
 	local -i octal i eggs=0
-	octal=$(bc <<<"obase=8; $1")
+	printf -v octal "%o\n" "$1"
 	for ((i = 0; i < ${#octal}; i++)); do
 		((eggs += oct_2_bin[${octal:i:1}]))
 	done
