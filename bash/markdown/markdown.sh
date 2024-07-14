c7a0ce4dace2401ca55e821fb14422eb72cf80d5
Vincent De Comarmond <vincent.de.comarmond.sa@gmail.com>
2024-07-14 21:55:16 +0200

    Handle headers better

--- bash/markdown/markdown.sh
+++ bash/markdown/markdown.sh
@@ -7,9 +7,10 @@
 # 2. put in main function
 # 3. Made separate function for replacing strongs
 # 4. Removed weird X$var = X tests
+# 5. Made a seperate function for replacing emphasis and replaced usages thereof in main
 
 replace_strong() {
-	local line="$1" pre post
+	local line="$1"
 	if [[ "$line" =~ ^(.*)__(.*)__(.*) ]]; then
 		printf -v line "%s<strong>%s</strong>%s" "${BASH_REMATCH[1]}" "${BASH_REMATCH[2]}" "${BASH_REMATCH[3]}"
 		replace_strong "$line"
@@ -32,42 +33,30 @@ main() {
 	local inside_a_list=""
 	while IFS= read -r line; do
 		line=$(replace_strong "$line")
+		line=$(replace_emphasis "$line")
 
 		if [[ "$line" =~ ^\* ]]; then
 			if [ "$inside_a_list" != yes ]; then
 				h="$h<ul>"
 				inside_a_list=yes
 			fi
-			line=$(replace_emphasis "$line")
-			h="$h<li>${line#??}</li>" # Strips next 2 vcharacters
+			h="$h<li>${line#??}</li>" # Strips 2 characters following line (why?)
+			continue
+		fi
 
-		else
-			if [ "$inside_a_list" == yes ]; then
-				h="$h</ul>"
-				inside_a_list=no
-			fi
+		if [ "$inside_a_list" == yes ]; then
+			h="$h</ul>"
+			inside_a_list=no
+		fi
 
-			n=$(expr "$line" : "#\{1,\}")
-			if [ $n -gt 0 -a 7 -gt $n ]; then
-
-				while [[ $line == *_*?_* ]]; do
-					s=${line#*_}
-					t=${s#*_}
-					if [ ${#t} -lt ${#s} -a ${#s} -lt ${#line} ]; then
-						line="${line%%_$s}<em>${s%%_$t}</em>$t"
-					fi
-				done
-				HEAD=${line:n}
-				while [[ $HEAD == " "* ]]; do HEAD=${HEAD# }; done
-				h="$h<h$n>$HEAD</h$n>"
-
-			else
-
-				grep '_..*_' <<<"$line" >/dev/null &&
-					line=$(echo "$line" | sed -E 's,_([^_]+)_,<em>\1</em>,g')
-				h="$h<p>$line</p>"
+		if [[ "$line" =~ ^(#*)[[:space:]](.*) ]]; then
+			if [ "${#BASH_REMATCH[1]}" -lt 7 ]; then
+				h="$h<h${#BASH_REMATCH[1]}>${BASH_REMATCH[2]}</h${#BASH_REMATCH[1]}>"
+				continue
 			fi
 		fi
+		h="$h<p>$line</p>"
+
 	done <"$1"
 
 	if [ "$inside_a_list" == yes ]; then
--- bash/markdown/test.md
+++
@@ -1,3 +0,0 @@
-# Header!
-* __Bold Item__
-* _Italic Item_
