2f62ee97c124dd9868abf760b658e712fdd13644
Vincent De Comarmond <vincent.de.comarmond.sa@gmail.com>
2024-07-25 17:25:32 +0200

    Did (tried) hellman

--- bash/diffie-hellman/diffie_hellman.sh
+++ bash/diffie-hellman/diffie_hellman.sh
@@ -1,32 +1,3 @@
 #!/usr/bin/env bash
-_generatePrimes() {
-	local -i i j stop isprime=0
-	local -a primes=(2)
-	stop="$(bc <<<"sqrt($1)")"
-	for ((i = 3; i <= $stop; i += 1)); do
-		isprime=1
-		for j in "${primes[@]}"; do
-			if ((i % j == 0)); then isprime=0 && break; fi
-		done
-		if ((isprime == 1)); then primes+=("$i"); fi
-	done
-	echo "${primes[@]}"
-}
-
-privateKey() {
-	local -a primes
-	read -r -a primes <<<"$(_generatePrimes "$1")"
-	echo "${primes[$RANDOM % ${#primes[@]}]}"
-}
-
-main() {
-	if [ "$1" == "privateKey" ]; then
-		privateKey "$2"
-	elif [ "$1" == "publicKey" ]; then
-		echo $((($3 ** $4) % $2))
-	elif [ "$1" == "secret" ]; then
-		echo $((($3 ** $4) % $2))
-	fi
-}
-
-main "$@"
+# Use bc external tool or suffer from overflow error
+if [ "$1" == "privateKey" ]; then echo $((2 + $RANDOM % ($2 - 2))); else bc <<<"scale=0;$3^$4%$2"; fi
