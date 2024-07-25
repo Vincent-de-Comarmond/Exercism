88ca112caedc6c47539ce7bba3ec5461cbe38938
Vincent De Comarmond <vincent.de.comarmond.sa@gmail.com>
2024-07-25 17:26:38 +0200

    Removed unneeded $ (shellcheck ask)

--- bash/diffie-hellman/diffie_hellman.sh
+++ bash/diffie-hellman/diffie_hellman.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Use bc external tool or suffer from overflow error
-if [ "$1" == "privateKey" ]; then echo $((2 + $RANDOM % ($2 - 2))); else bc <<<"scale=0;$3^$4%$2"; fi
+if [ "$1" == "privateKey" ]; then echo $((2 + RANDOM % ($2 - 2))); else bc <<<"scale=0;$3^$4%$2"; fi
