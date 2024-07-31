05b2dfaf5ee8ed6a8bee7f885184a378c3135931
Vincent De Comarmond <vincent.de.comarmond.sa@gmail.com>
2024-07-31 22:02:27 +0200

    Make shellcheck happy

--- bash/crypto-square/crypto_square.sh
+++ bash/crypto-square/crypto_square.sh
@@ -2,7 +2,7 @@
 main() {
 	local normalized coded
 	local -i idx rows=0 cols=0 r=0 c=0
-	read -r normalized < <(echo "$1" | tr [[:upper:]] [[:lower:]] | tr -cd [[:lower:][0-9]])
+	read -r normalized < <(echo "$1" | tr '[:upper:]' '[:lower:]' | tr -cd '[[:lower:][0-9]]')
 	while ((rows * cols < ${#normalized})); do
 		((cols++))
 		if ((rows * cols >= ${#normalized})); then break; else ((rows++)); fi
@@ -10,7 +10,7 @@ main() {
 	printf -v normalized "%-*s" $((rows * cols)) "$normalized"
 	for ((c = 0; c < cols; c++)); do
 		for ((r = 0; r < rows; r++)); do
-			(((idx = r * cols + c)))
+			((idx = r * cols + c))
 			coded="$coded${normalized:idx:1}"
 		done
 		if ((c < cols - 1)); then coded="$coded "; fi
