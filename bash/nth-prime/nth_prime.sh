3d4f78c850f3d03e3eea01ee78305f74e8b8db3c
Vincent De Comarmond <vincent.de.comarmond.sa@gmail.com>
2024-08-03 21:13:02 +0200

    Make shellcheck happy

--- bash/nth-prime/nth_prime.sh
+++ bash/nth-prime/nth_prime.sh
@@ -3,13 +3,13 @@ main() {
 	if [ "$1" -le 0 ]; then echo "invalid input" >/dev/stderr && exit 1; fi
 
 	local -a primes=(2 3)
-	local -i prime isprime=1 test_p=3
+	local -i isprime=1 test_p=3
 	if (($1 <= 2)); then echo "${primes[$1 - 1]}" && return; fi
 	while ((${#primes[@]} < $1)); do
 		isprime=1
 		((test_p += 2))
 		for ((i = 0; primes[i] * primes[i] <= test_p; i++)); do
-			if ((test_p % ${primes[i]} == 0)); then
+			if ((test_p % primes[i] == 0)); then
 				isprime=0
 				break
 			fi
