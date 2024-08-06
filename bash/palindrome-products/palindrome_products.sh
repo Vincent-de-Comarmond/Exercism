5528eee5eabd9c227e18d3c593605145784ada91
Vincent De Comarmond <vincent.de.comarmond.sa@gmail.com>
2024-08-06 15:05:16 +0200

    More work on pallindrome products

--- bash/palindrome-products/palindrome_products.sh
+++ bash/palindrome-products/palindrome_products.sh
@@ -1,53 +1,65 @@
 #!/usr/bin/env bash
-is_pallindrome() {
-	local alpha beta half
-	((half = ${#1} / 2))
-	alpha="${1:0:$half}"
-	if ((${#1} % 2 == 0)); then beta="${1:$half}"; else beta="${1:$((half + 1))}"; fi
-	read -r beta < <(rev <<<"$beta")
-	if [ "$alpha" == "$beta" ]; then return 0; fi
-	return 1
-}
+generate_palindromes() {
+	local -i i idx min max digits
+	local alphabet=abcdefghijklmnopqrstuvwxyz start middle="" end tmp tmp2
+	local -a palindromes
+	((min = $1 * $1, max = $2 * $2))
+	((digits = ${#max}))
+	echo "min: $min"
+	echo "max: $max"
+	echo "digits: $digits"
+	start="${alphabet:0:$((digits / 2))}"
+	read -r end < <(rev <<<"$start")
+	if ((digits % 2 == 1)); then middle="${alphabet:$((digits / 2)):1}"; fi
 
-pallindrome_products() {
-	local -i i j product
-	local -a products
-	for ((i = $1; i <= $2; i++)); do
-		for ((j = i; j <= $2; j++)); do
-			((product = i * j))
-			if is_pallindrome "$product"; then products+=("$product"); fi
+	palindromes+=("$start$middle$end")
+	while [[ "${palindromes[*]}" =~ [a-z] ]]; do
+		for idx in "${!palindromes[@]}"; do
+			tmp="${palindromes[$idx]}"
+			tmp2="${tmp//[0-9]/}"
+			if [ "${tmp2:0:1}" == "" ]; then break; fi
+			for i in {0..9}; do palindromes+=("${tmp//${tmp2:0:1}/$i}"); done
+			unset "palindromes[$idx]"
 		done
 	done
-	echo "${products[@]}"
-}
 
-ordered_products() {
-	local -i i="$2" j="$2"
+	# No palindromes that are legitimately bounded by 0s
+	# for idx in "${!palindromes[@]}"; do
+	# 	if [[ "${palindromes[$idx]}" =~ ^0+(.*)0+$ ]]; then
+	# 		unset "palindromes[$idx]"
+	# 		if [ "${BASH_REMATCH[1]}" != "" ]; then palindromes+=("${BASH_REMATCH[1]}"); fi
+	# 	fi
+	# done
 
-	while ((i >= $1 && j >= $1)); do
-		echo $((i * j))
-		if ((i > j)); then ((i--)); else ((j--)); fi
-	done
+	for tmp in "${palindromes[@]}"; do ((min <= tmp && tmp <= max)) && echo "$tmp"; done | sort -n
 }
 
 main() {
-	local -i product=1 result i
-	local factor_string=""
-	local -a products factors
-	local -A uniq_factors
-	read -r -a products < <(pallindrome_products "$2" "$3")
-	if [ "$1" == smallest ]; then result="${products[0]}"; else result="${products[-1]}"; fi
-	read -r -a factors < <(factor "$result" | cut -d: -f2-)
-	for i in "${factors[@]}"; do
-		((product *= i))
-		uniq_factors["$i"]=1
+	local -i gotcha=0 i j palindrome smaller larger
+	local factor_string
+	local -a palindromes
+	local -A factors
+	# generate_palindromes "$2" "$3"
+	if [ "$1" == smallest ]; then
+		readarray -t palindromes < <(generate_palindromes "$2" "$3")
+	else
+		readarray -t palindromes < <(generate_palindromes "$2" "$3" | rev)
+	fi
+
+	for palindrome in "${palindromes[@]}"; do
+		factors=()
+		for ((i = $2; i <= $3; i++)); do
+			((j = palindrome / i))
+			if ((palindrome % i == 0 && $2 <= j && j <= $3)); then
+				((smaller = i <= j ? i : j, larger = i > j ? i : j))
+				gotcha=1
+				if [[ "$factor_string" != *"[$smaller, $larger]"* ]]; then factor_string="$factor_string [$smaller, $larger]"; fi
+			fi
+		done
+		if ((gotcha == 1)); then break; fi
 	done
-	uniq_factors[1]=1
-	
-	for i in "${!uniq_factors[@]}"; do
-		factor_string="$factor_string [$i, $((product / i))]"
-	done
-	echo "$result:$factor_string"
+
+	echo "${palindrome# }:$factor_string"
 }
 
 main "$@"
