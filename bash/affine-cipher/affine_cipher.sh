0f32b9a9e7101a976d66025fcb3572d48ac32841
Vincent De Comarmond <vincent.de.comarmond.sa@gmail.com>
2024-08-09 14:05:40 +0200

    Manged affine cipher

--- bash/affine-cipher/affine_cipher.sh
+++ bash/affine-cipher/affine_cipher.sh
@@ -1,52 +1,47 @@
 #!/usr/bin/env bash
+die() { echo "$1" >/dev/stderr && exit 1; }
 
-encode() {
-	local -i ord a="$1" b="$2"
-	LC_CTYPE=C printf -v ord '%d' "'$3"
-	((ord = (a * (ord - 97) + b) % 26)) # ascii "a" is 97
+ext_euclid() { # https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
+	local -i r0="$1" r1="$2" s0="${3:-1}" s1="${4:-0}" gcd mmi
+	if ((r0 == 0 || r1 == 0)); then
+		((gcd = r0 != 0 ? r0 : r1, mmi = r0 != 0 ? s0 : s1))
+		echo "$gcd" "$((mmi = mmi < 0 ? mmi % 26 + 26 : mmi))" && return
+	fi
+	((r2 = r1 - (r1 / r0) * r0, s2 = s1 - (r1 / r0) * s0))
+	ext_euclid "$r2" "$r0" "$s2" "$s0"
+}
+
+to_ord() {
+	local -i ord
+	LC_CTYPE=C printf -v ord '%d' "'$1"
+	echo -n "$((ord - 97))" # The 97 offset is the reason I usually use arrays/associative-arrays for this stuff
+}
+
+to_char() {
+	local -i ord="$1"
 	((ord += 97))
-	printf -v ord "%03o" "$ord"
-	printf "\\$ord"
-}
-
-decode() {
-	local -i ord a="$1" a_inv b="$2"
-	LC_CTYPE=C printf -v ord '%d' "'$3"
-	((ord -= (b + 97)))
-	read -r a_inv < <(mmi "$a")
-	((ord = (a_inv * ord) % 26 + 97))
-	printf -v ord "%03o" "$ord"
-	printf "\\$ord"
-}
-
-eea() { # https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
-	local -i r0="$1" r1="$2" s0="${3:-1}" s1="${4:-0}" q
-	if ((r0 != 0 && r1 != 0)); then
-		((q = r1 / r0))
-		((r2 = r1 - q * r0, s2 = s1 - q * s0))
-		eea "$r2" "$r0" "$s2" "$s0"
-	fi
-	echo "$((r0 != 0 ? r0 : r1))" "$((r0 != 0 ? s0 : s1))"
-}
-
-mmi() {
-	local -i gcd _mmi
-	read -r gcd _mmi < <(eea "$1" 26)
-	if ((gcd != 1)); then
-		echo "A and m are not coprime. No solution" >/dev/stderr
-		exit 1
-	fi
-	((_mmi = _mmi < 0 ? (_mmi % 26) + 26 : _mmi))
-	echo "$mmi"
+	printf "\\$(printf "%03o" "$ord")"
 }
 
 main() {
-	local i input="${4,,}"
-	input="${input// /}"
+	local -i i a="$2" b="$3" m=26 gcd mmi ord
+	local input="${4,,}" output="" char=""
+
+	read -r input < <(tr -cd "a-z0-9" <<<"$input")
+	read -r gcd mmi < <(ext_euclid "$a" "$m") # For encoding this just tests if they're relatively prime
+	if ((gcd != 1)); then die "a and m must be coprime."; fi
 
 	for ((i = 0; i < ${#input}; i++)); do
-		"$1" "$2" "$3" "${input:$i:1}"
+		char="${input:$i:1}"
+		if [[ "$char" =~ [a-z] ]]; then
+			read -r ord < <(to_ord "$char")
+			if [ "$1" == "encode" ]; then ((ord = (a * ord + b) % 26)); else ((ord = mmi * (ord - b) % 26)); fi
+			read -r char < <(to_char "$((ord < 0 ? ord + 26 : ord))")
+		fi
+		if [ "$1" == encode ] && ((0 < i && i % 5 == 0)); then char=" $char"; fi
+		output="$output$char"
 	done
+	echo "$output"
 }
 
 main "$@"
