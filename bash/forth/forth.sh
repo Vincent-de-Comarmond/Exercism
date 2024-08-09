c707d6cf54be041c4423f28ae6116ed09027bb2a
Vincent De Comarmond <vincent.de.comarmond.sa@gmail.com>
2024-08-09 21:32:04 +0200

    Implemented forth

--- bash/forth/forth.sh
+++ bash/forth/forth.sh
@@ -1,47 +1,79 @@
 #!/usr/bin/env bash
 die() { echo "$1" >/dev/stderr && exit 1; }
 
-forth() {
-	local arg tmp1 tmp2
-	local -a input=() stack=()
-	read -r -a input <"${@:-/dev/stdin}"
+process() {
+	local arg tmp
+	local -a stack # Can make global if needed
+	local -a one_arg_ops=("dup" "drop") two_arg_ops=("+" "-" "*" "/" "swap" "over")
+	for arg in "$@"; do
+		if [[ "$arg" =~ ^-?[0-9]+$ ]]; then stack+=("$arg") && continue; fi
 
-	for arg in "${input[@]}"; do
-		if [[ "$arg" =~ ^-?[0-9]+$ ]]; then
-			stack+=("$arg")
-		elif [[ "$arg" == "+" || "$arg" == "-" || "$arg" == "*" || "$arg" == "/" ]]; then
-			if ((${#stack[@]} == 0)); then die "empty stack"; fi
-			if ((${#stack[@]} == 1)); then die "only one value on the stack"; fi
-			if ((stack[-1] == 0)) && [ "$arg" == "/" ]; then die "divide by zero"; fi
+		# Check if valid
+		if [[ "${one_arg_ops[*]}" != *"$arg"* && "${two_arg_ops[*]}" != *"$arg"* ]]; then die "undefined operation"; fi
+		if ((${#stack[@]} == 0)); then die "empty stack"; fi
+		if [[ "$arg" == "/" && "${stack[-1]}" -eq 0 ]]; then die "divide by zero"; fi
+		if [[ "${two_arg_ops[*]}" == *"$arg"* && "${#stack[@]}" -eq 1 ]]; then die "only one value on the stack"; fi
 
-			if [ "$arg" == "+" ]; then ((tmp1 = stack[-2] + stack[-1])); fi
-			if [ "$arg" == "-" ]; then ((tmp1 = stack[-2] - stack[-1])); fi
-			if [ "$arg" == "*" ]; then ((tmp1 = stack[-2] * stack[-1])); fi
-			if [ "$arg" == "/" ]; then ((tmp1 = stack[-2] / stack[-1])); fi
-
+		case "$arg" in
+		"+" | "-" | "*" | "/")
+			case "$arg" in
+			"+") ((tmp = stack[-2] + stack[-1])) ;;
+			"-") ((tmp = stack[-2] - stack[-1])) ;;
+			"*") ((tmp = stack[-2] * stack[-1])) ;;
+			"/") ((tmp = stack[-2] / stack[-1])) ;;
+			esac
 			unset "stack[-1]"
 			unset "stack[-1]"
-			stack+=("$tmp1")
-		elif [ "$arg" == "dup" ]; then
-			if ((${#stack[@]} == 0)); then die "empty stack"; fi
-			stack+=("${stack[-1]}")
-		elif [ "$arg" == "drop" ]; then
-			if ((${#stack[@]} == 0)); then die "empty stack"; fi
-			unset "stack[-1]"
-		elif [ "$arg" == "swap" ]; then
-			if ((${#stack[@]} == 0)); then die "empty stack"; fi
-			if ((${#stack[@]} == 1)); then die "only one value on the stack"; fi
-			((tmp1 = stack[-2]))
+			stack+=("$tmp")
+			;;
+		"dup") stack+=("${stack[-1]}") ;;
+		"drop") unset "stack[-1]" ;;
+		"swap")
+			((tmp = stack[-2]))
 			stack[-2]="${stack[-1]}"
-			stack[-1]="$tmp1"
-		elif [ "$arg" == "over" ]; then
-			if ((${#stack[@]} == 0)); then die "empty stack"; fi
-			if ((${#stack[@]} == 1)); then die "only one value on the stack"; fi
-			stack+=("${stack[-2]}")
-		fi
+			stack[-1]="$tmp"
+			;;
+		"over") stack+=("${stack[-2]}") ;;
+		esac
+
 	done
 
 	echo "${stack[@]}"
+}
+
+forth() {
+	local line tmp1 tmp2 key replacement
+	local -A macros=()
+	#################
+	# Handle Macros #
+	#################
+	while read -r line && test -n "$line"; do
+		line="${line,,}"
+
+		if [[ "$line" =~ ^: ]]; then
+			read -r -a tmp1 <<<"$line"
+			if [ "${tmp1[-1]}" != ";" ]; then die "macro not terminated with semicolon"; fi
+			if [ "${#tmp1[@]}" -le 3 ]; then die "empty macro definition"; fi
+			if [[ "${tmp1[1]}" =~ -?[0-9]+ ]]; then die "illegal operation"; fi
+			tmp2="${tmp1[@]:2:${#tmp1[@]}-3}"
+
+			for key in "${!macros[@]}"; do
+				replacement="${macros[$key]}"
+				tmp2="${tmp2//$key/$replacement}"
+			done
+
+			macros["${tmp1[1]}"]="$tmp2"
+			continue
+		else
+			for key in "${!macros[@]}"; do
+				replacement="${macros[$key]}"
+				line="${line//$key/$replacement}"
+			done
+			read -r -a tmp1 <<<"$line"
+		fi
+
+		process "${tmp1[@]}"
+	done <"${@:-/dev/stdin}"
 }
 
 forth "$@"
