39afbdf71869917125662a52b6f2a02f9608afdc
Vincent De Comarmond <vincent.de.comarmond.sa@gmail.com>
2024-08-27 17:34:51 +0200

    Linearization doesn't work

--- bash/knapsack/knapsack.sh
+++ bash/knapsack/knapsack.sh
@@ -2,76 +2,45 @@
 print_arr() {
 	local -n _printme="$1"
 	local _key
-	for _key in "${!_printme[@]}"; do echo -e "$_key)\t${_printme[$_key]}"; done
+	for _key in "${!_printme[@]}"; do echo "$_key ${_printme[$_key]}"; done
 }
 
-arr_max() {
-	local -i __tmp _max=-1000000
-	for __tmp in "$@"; do ((_max = __tmp > _max ? __tmp : _max)); done
-	echo "$_max"
-}
-
-copy_omit() {
-	local -n cpy="$1"
-	local -a tmp=()
-	local -i _idx omit="$2"
-	for _idx in "${!cpy[@]}"; do if ((_idx != omit)); then tmp+=("${cpy[$_idx]}"); fi; done
-	echo "${tmp[*]}"
-}
-
-pack_bag() {
-	local -i i j _c weight val modified=0
-	local -a w v c s
-
-	read -r -a w <<<"${weights[0]}"
-
-	for ((_c = 0; _c < ${#w[@]} + 1; _c++)); do
-		for i in "${!weights[@]}"; do
-			read -r -a w <<<"${weights[$i]}"
-			read -r -a v <<<"${values[$i]}"
-			read -r -a c <<<"${capacities[$i]}"
-			read -r -a s <<<"${scores[$i]}"
-
-			modified=0
-			for j in "${!w[@]}"; do
-				weight="${w[$j]}"
-				val="${v[$j]}"
-				if ((weight <= c)); then
-					modified=1
-					scores+=($((s + val)))
-					capacities+=($((c - weight)))
-					weights+=("$(copy_omit w "$j")")
-					values+=("$(copy_omit v "$j")")
-				fi
-			done
-			if ((modified == 1)); then
-				unset weights["$i"]
-				unset values["$i"]
-				unset capacities["$i"]
-				unset scores["$i"]
-			fi
-		done
-	done
+resort_arr() {
+	local idx
+	local -n _sortme="$1" _sortby="$2"
+	local -a _tmp=()
+	readarray -t _tmp < <(for idx in "${_sortby[@]}"; do echo "${_sortme[$idx]}"; done)
+	echo "${_tmp[@]}"
 }
 
 main() {
 	if (($# < 1)); then echo "Incorrent number of arguments" && exit 1; fi
 
-	local -i idx
-	local -a w__v tmp_w tmp_v
-	local w_str v_str
+	local i _str val wgt den capacity="$1" score=0
+	local -a wv_str weights values density sorted_idx
 
-	read -r -a w__v < <(tr ":" " " <<<"${@:2}")
-	for ((idx = 0; idx < ${#w__v[@]}; idx += 2)); do
-		tmp_w+=("${w__v[$idx]}")
-		tmp_v+=("${w__v[$idx + 1]}")
+	read -r -a wv_str < <(tr ":" " " <<<"${@:2}")
+	for ((i = 0; i < ${#wv_str[@]}; i += 2)); do
+		wgt="${wv_str[$i]}"
+		val="${wv_str[$i + 1]}"
+		read -r den < <(bc -l <<<"$val/$wgt")
+		weights+=("$wgt")
+		values+=("$val")
+		density+=("$den")
 	done
 
-	w_str="${tmp_w[*]}"
-	v_str="${tmp_v[*]}"
-	declare -ag weights=("$w_str") values=("$v_str") capacities=("$1") scores=(0)
-	pack_bag
-	arr_max "${scores[@]}"
+	readarray -t sorted_idx < <(print_arr density | sort -g -k2 -r | cut -d " " -f1)
+	read -r -a weights < <(resort_arr weights sorted_idx)
+	read -r -a values < <(resort_arr values sorted_idx)
+	read -r -a density < <(resort_arr density sorted_idx)
+
+	for ((i = 0; i < ${#values[@]}; i++)); do
+		if ((weights[i] <= capacity)); then
+			((score += values[i]))
+			((capacity -= weights[i]))
+		fi
+	done
+	echo "$score"
 }
 
 main "$@"
