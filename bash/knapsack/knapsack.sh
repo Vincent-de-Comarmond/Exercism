b16e982a83db9ad1929c6595c9e9b76d3e4303b8
Vincent De Comarmond <vincent.de.comarmond.sa@gmail.com>
2024-08-28 16:54:44 +0200

    Heaps algorithm doesn't help

--- bash/knapsack/knapsack.sh
+++ bash/knapsack/knapsack.sh
@@ -5,42 +5,69 @@ print_arr() {
 	for _key in "${!_printme[@]}"; do echo "$_key ${_printme[$_key]}"; done
 }
 
-resort_arr() {
-	local idx
-	local -n _sortme="$1" _sortby="$2"
-	local -a _tmp=()
-	readarray -t _tmp < <(for idx in "${_sortby[@]}"; do echo "${_sortme[$idx]}"; done)
-	echo "${_tmp[@]}"
-}
+# choose() {
+# 	local -i i n="$1" r="$2"
+# 	local _str=""
+# 	local -a tmp choice_ensemble choices
+
+# 	read -r -a tmp < <(for ((i = 0; i < n; i++)); do echo "$i"; done)
+
+# 	for ((i = 0; i < r; i++)); do
+# 	done
+# }
 
 main() {
 	if (($# < 1)); then echo "Incorrent number of arguments" && exit 1; fi
 
-	local i _str val wgt den capacity="$1" score=0
-	local -a wv_str weights values density sorted_idx
+	declare -Ag choice_cache=()
+	local i _str tmp1 tmp2 min_item=0 max_item=0 capacity="$1" score=0
+	local -a wv_str weights values weights_incr weights_decr
 
 	read -r -a wv_str < <(tr ":" " " <<<"${@:2}")
 	for ((i = 0; i < ${#wv_str[@]}; i += 2)); do
-		wgt="${wv_str[$i]}"
-		val="${wv_str[$i + 1]}"
-		read -r den < <(bc -l <<<"$val/$wgt")
-		weights+=("$wgt")
-		values+=("$val")
-		density+=("$den")
+		weights+=("${wv_str[$i]}")
+		values+=("${wv_str[$i + 1]}")
 	done
 
-	readarray -t sorted_idx < <(print_arr density | sort -g -k2 -r | cut -d " " -f1)
-	read -r -a weights < <(resort_arr weights sorted_idx)
-	read -r -a values < <(resort_arr values sorted_idx)
-	read -r -a density < <(resort_arr density sorted_idx)
+	readarray -t weights_incr < <(for _str in "${weights[@]}"; do echo "$_str"; done | sort -n)
+	readarray -t weights_decr < <(for _str in "${weights[@]}"; do echo "$_str"; done | sort -n -r)
 
-	for ((i = 0; i < ${#values[@]}; i++)); do
-		if ((weights[i] <= capacity)); then
-			((score += values[i]))
-			((capacity -= weights[i]))
-		fi
+	((tmp1 = capacity, tmp2 = capacity))
+	for i in "${!weights[@]}"; do
+		if ((weights_incr[i] <= tmp1)); then ((tmp1 -= weights_incr[i], max_item++)); fi
+		if ((weights_decr[i] <= tmp2)); then ((tmp2 -= weights_decr[i], min_item++)); fi
 	done
-	echo "$score"
+
+	echo "Max items: $max_item"
+	echo "Min items: $min_item"
+
 }
 
-main "$@"
+# main "$@"
+
+# declare -a array
+# choose 5 2 array
+
+declare -a array
+readarray -t array < <(for ((i = 0; i < 15; i++)); do echo "$i"; done)
+echo "${array[@]}"
+
+heaps() {
+	local -i i a b idx
+	local -a A=("${@:2}")
+	if (($1 == 1)); then
+		echo "${A[@]}"
+	else
+		heaps $(($1 - 1)) "${A[@]}"
+		for ((i = 0; i < $1 - 1; i++)); do
+			((idx = $1 % 2 == 0 ? i : 0))
+			((a = A[idx]))
+			((b = A[$1 - 1]))
+			A["$idx"]="$b"
+			A["$(($1 - 1))"]="$a"
+			heaps $(($1 - 1)) "${A[@]}"
+		done
+	fi
+}
+
+heaps "${#array[@]}" "${array[@]}"
